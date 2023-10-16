6d65e50bb852d74dc2110c94fe2a8bc1eb2287ed
pallavi <pallavirkg2004@gmail.com>
2023-10-16 10:38:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [size, setSize] = useState(0);
+
+  let change = (e) => {
+    wordcount(e.target.value);
+  };
+
+  function wordcount(str) {
+    let c = 0;
+    let str1 = str.split(" ");
+
+    for (let i = 0; i < str1.length; i++) {
+      if (str1.length === 0) {
+        setCount(0);
+      }
+      if (str1[i] !== "") {
+        c++;
+      }
+      setCount(c);
+    }
+  }
+
+
+
+  const sizechange = (e) => {
+    setSize(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="file">
+        <h1>Responsive Paragraph Word Counter</h1>
+        <br />
+        <textarea
+          onChange={change}
+        ></textarea>
+        <br />
+        <div id="word-counter">Word Count:{count}</div>
+        <br />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
